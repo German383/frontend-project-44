ab6dfbba90de5a8bdf043a1b436784159962c01c
German383 <Serbakov.ga@gmail.com>
2023-05-27 14:50:47 +0300

    finish 4 step

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!')
+import showGreetings from "../src/cli.js";
+
+showGreetings();
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import { question } from "readline-sync";
+
+const showGreetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const name = question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export default showGreetings;
